202b13dea6a05c70fdbbc4ea5a184dedd7003474
Dan Drew <dan.drew@hotmail.com>
2024-01-05 15:29:20 -0800

    Add server side encryption to prevent unnecessary state diffs

--- main.tf
+++ main.tf
@@ -28,5 +28,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
   bucket = aws_s3_bucket.bucket.id
   rule {
     bucket_key_enabled = true
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
